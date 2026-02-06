4e630e796795ee90b1b63cb4d4b57338c586ebff
masters-del <szarubin24@gmail.com>
2026-02-06 02:48:44 +0200

    Fix: updated libs in start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,15 @@
 @echo off
-title UserBot Starter
-echo Проверка библиотек...
-pip install telethon python-dotenv requests
+title UserBot Starter v6.2
+chcp 65001 >nul
+color 0b
+
+echo 🔍 Проверка и установка библиотек...
+:: Авто-установка всех нужных либ
+python -m pip install --upgrade pip
+pip install telethon python-dotenv gtts requests pillow
+
 cls
+echo ✅ Библиотеки проверены! Запускаю бота...
+echo ---------------------------------------
 python main.py
 pause
